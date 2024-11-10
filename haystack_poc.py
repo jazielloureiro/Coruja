faf468c831576edc07478e6ab7a464540b6d17a0
jazielloureiro <jazielloureiro17@gmail.com>
2024-11-10 13:26:06 -0300

    Add Ollama Embedder in the Haystack pipeline POC

--- haystack_poc.py
+++ haystack_poc.py
@@ -2,14 +2,24 @@ from haystack import Pipeline
 from haystack.components.converters import TikaDocumentConverter
 from haystack.components.preprocessors import DocumentCleaner
 from haystack.components.preprocessors import DocumentSplitter
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack.document_stores.types import DuplicatePolicy
+from haystack.components.writers import DocumentWriter
+from haystack_integrations.components.embedders.ollama import OllamaDocumentEmbedder
+
+document_store = InMemoryDocumentStore(embedding_similarity_function='cosine')
 
 pipeline = Pipeline()
 
-pipeline.add_component("converter", TikaDocumentConverter())
-pipeline.add_component("cleaner", DocumentCleaner())
-pipeline.add_component("splitter", DocumentSplitter(split_by="sentence", split_length=5))
+pipeline.add_component('converter', TikaDocumentConverter())
+pipeline.add_component('cleaner', DocumentCleaner())
+pipeline.add_component('splitter', DocumentSplitter(split_by='sentence', split_length=5))
+pipeline.add_component('embedder', OllamaDocumentEmbedder())
+pipeline.add_component('writer', DocumentWriter(document_store=document_store, policy=DuplicatePolicy.OVERWRITE))
 
-pipeline.connect("converter", "cleaner")
-pipeline.connect("cleaner", "splitter")
+pipeline.connect('converter', 'cleaner')
+pipeline.connect('cleaner', 'splitter')
+pipeline.connect('splitter', 'embedder')
+pipeline.connect('embedder', 'writer')
 
-print(pipeline.run({"converter": {"sources": ['./test.pdf']}}))
+print(pipeline.run({'converter': {'sources': ['./test.pdf']}}))
